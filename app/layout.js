6eb7ebde2359d7295eb66708ad5e7f397d2cbbc7
admisigered <administradorsigered@gmail.com>
2026-04-29 21:37:01 -0500

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,13 @@
+import './globals.css'
+
+export const metadata = {
+  title: 'SIGERED - Sistema de Recuperación',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="es">
+      <body>{children}</body>
+    </html>
+  )
+}
